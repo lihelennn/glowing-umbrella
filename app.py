6cd957f8978840059b2e7458ba0ff65e11b45356
HELEN LI <helen82198@gmail.com>
2017-01-08 12:25:54 -0500

    figured out zip code for past

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def home():
         print "yooyooooo"
         return render_template("home.html")
     else:
-        location = [35,140]
-        #location = request.form["javascript_data"]
+        #location = [30, -10]
+        location = request.form["javascript_data"]
         if location == None:
             location = [40.71, -74.01]
             msg = "We cannot detect where you are, but here are the temperatures for 10002, USA"
@@ -24,9 +24,21 @@ def home():
         return redirect(url_for('climate',lat=location[0], lon=location[1]))
 
 
+#use api to search location
+#load information into climate
+#loads the current/past temperatures of the location
 @app.route('/climate', methods=['GET', 'POST'])
 def climate():
     msg=""
+    tempAvg2016=""
+    tempAvg2015=""
+    tempAvg2014=""
+    tempAvg2013=""
+    tempAvg2012=""
+    tempAvg2011=""
+    tempAvg2010=""
+    tempAvg2005=""
+    tempAvg2000=""
     info = information.getRandomInfo()
     if request.args.get("information") == "information":
         info = information.getRandomInfo()
@@ -39,33 +51,70 @@ def climate():
         temperature = 0
         latitude = request.args.get("lat")
         longitude = request.args.get("lon")
-        urlZip = "http://ziplocate.us/api/v1/reverse/latitude,longitude"
-        urlZip=urlZip.replace("latitude", latitude)
-        urlZip=urlZip.replace("longitude", longitude)
-        req = urllib2.urlopen(urlZip)
+        #urlZip = "www.ziplocate.us/api/v1/reverse/latitude,longitude"
+        #urlZip=urlZip.replace("latitude", latitude)
+        #urlZip=urlZip.replace("longitude", longitude)
+        #print urlZip2
+        #c = httplib.HTTPConnection(urlZip)
+        #c.request("HEAD", '')
+        #print c.getresponse().status
+        #if c.getresponse().status < 400:
+        #    print "YOOOOO"
+        try:
+            urlZip="http://ziplocate.us/api/v1/reverse/latitude,longitude"
+            urlZip=urlZip.replace("latitude", latitude)
+            urlZip=urlZip.replace("longitude", longitude)
+            print urlZip
+            req = urllib2.urlopen(urlZip)
+            result = req.read()
+            r = json.loads(result)
+            zipcode = r["zip"]
+            print zipcode
+            for i in range(len(fives)):
+                historyYear = fives[i]
+                urlHistory= "https://api.weathersource.com/v1/5f7b808d57d0226ddfa9/history_by_postal_code.json?period=day&postal_code_eq=zipcode&country_eq=US&timestamp_eq=YEAR-02-02-10T00:00:00-05:00&fields=tempAvg"
+                urlHistory=urlHistory.replace("zipcode", zipcode)
+                urlHistory=urlHistory.replace("YEAR", historyYear)
+                req = urllib2.urlopen(urlHistory)
+                result = req.read()
+                r = json.loads(result)
+                if r != []:
+                    r = r[0]
+                    for key in r.keys():
+                        if historyYear == "2016":
+                            tempAvg2016 = r[key]
+                        elif historyYear == "2015":
+                            tempAvg2015= r[key]
+                        elif historyYear == "2014":
+                            tempAvg2014 = r[key]
+                        elif historyYear == "2013":
+                            tempAvg2013 = r[key]
+                        elif historyYear == "2012":
+                            tempAvg2012 = r[key]
+                        elif historyYear == "2011":
+                            tempAvg2011 = r[key]
+                        elif historyYear == "2010":
+                            tempAvg2010 = r[key]
+                        elif historyYear == "2005":
+                            tempAvg2005 = r[key]
+                        elif historyYear == "2000":
+                            tempAvg2000 = r[key]
+        except:
+            print "No history for that location"
+
+
+        url="http://api.openweathermap.org/data/2.5/weather?lat=latitude&lon=longitude&units=imperial&appid=48d99f11447bdb9eeecf3dc47ecc0f57"
+        url=url.replace("latitude", latitude)
+        url=url.replace("longitude", longitude)
+        req = urllib2.urlopen(url)
         result = req.read()
         r = json.loads(result)
-        zipcode = r["zip"]
-
-        #use api to search location
-        #load information into climate
-
-        #url="http://api.openweathermap.org/data/2.5/weather?lat=latitude&lon=longitude&units=imperial&appid=48d99f11447bdb9eeecf3dc47ecc0f57"
-        #url=url.replace("latitude", latitude)
-        #url=url.replace("longitude", longitude)
-        #req = urllib2.urlopen(url)
-        #result = req.read()
-        #r = json.loads(result)
-        #for key in r.keys():
-        #    if key == "main":
-        #        main = r[key]
-        #        temperature = main['temp']
-        #        return render_template("climate.html",temperature=temperature, msg=msg, info=info)
+        for key in r.keys():
+            if key == "main":
+                main = r[key]
+                temperature = main['temp']
+                return render_template("climate.html",temperature=temperature, msg=msg, tempAvg2016=tempAvg2016,tempAvg2015=tempAvg2015, tempAvg2014=tempAvg2014, tempAvg2013=tempAvg2013, tempAvg2012=tempAvg2012, tempAvg2011=tempAvg2011,tempAvg2010=tempAvg2010, tempAvg2005=tempAvg2005,tempAvg2000=tempAvg2000, info=info)
     else:
-        tempAvg2015=""
-        tempAvg2010=""
-        tempAvg2005=""
-        tempAvg2000=""
         zipcode=request.args.get("zip")
         #if zipcode == "":
         #    msg = "please submit a zipcode"
@@ -113,7 +162,6 @@ def climate():
             if key == "main":
                 main = r[key]
                 temperature = main['temp']
-                print "HELLO"
                 return render_template("climate.html",temperature=temperature, msg=msg, tempAvg2016=tempAvg2016,tempAvg2015=tempAvg2015, tempAvg2014=tempAvg2014, tempAvg2013=tempAvg2013, tempAvg2012=tempAvg2012, tempAvg2011=tempAvg2011,tempAvg2010=tempAvg2010, tempAvg2005=tempAvg2005,tempAvg2000=tempAvg2000, info=info)
         return render_template("home.html")
 
