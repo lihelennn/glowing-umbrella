bdbdaecb9572376d129cabbd8859ada8bfd5c774
HELEN LI <helen82198@gmail.com>
2017-01-07 20:38:13 -0500

    lol

--- app.py
+++ app.py
@@ -6,42 +6,44 @@ app = Flask(__name__)
 location=[]
 temperature=0
 
-@app.route('/', methods=['GET','POST'])
-@app.route('/index', methods=['GET','POST'])
-@app.route("/home", methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
 def home():
-    if request.method=="GET":
+    if request.args.get("submit") != "sub":
         return render_template("home.html")
     else:
-        print "222222222"
-        print "location"
-        print "ugh"
+        print "hello"
+        location = [1,1]
+        print location
+        return redirect(url_for('climate',lat=location[0], lon=location[1]))
 
-        location = request.form["javascript_data"]
-        if location == None:
-            print "YO"
-        else:
-            print "hi"
-        latitude = location[0]
-        longitude = location[1]
-        #use api to search location
-        #load information into climate
 
-        url="http://api.openweathermap.org/data/2.5/find?lat=latitude&lon=longitude&cnt=10&units=Imperial&appid=48d99f11447bdb9eeecf3dc47ecc0f57"
-        url=url.replace("latitude", latitude)
-        url=url.replace("longitude", longitude)
-        req = urllib2.urlopen(url)
-        result = req.read()
-        r = json.loads(result)
-
-        if r.has_key("main"):
-            main = r["main"]
-            temperature = main["temp"]
-        return render_template("climate.html",temperature=temperature)
-
-@app.route('/climate')
+@app.route('/climate', methods=['GET', 'POST'])
 def climate():
-    return render_template("climate.html", location=location)
+    temperature = 0
+    latitude = request.args.get("lat")
+    longitude = request.args.get("lon")
+    print latitude
+    print longitude
+    #use api to search location
+    #load information into climate
+
+    url="http://api.openweathermap.org/data/2.5/find?lat=latitude&lon=longitude&cnt=10&units=Imperial&appid=48d99f11447bdb9eeecf3dc47ecc0f57"
+    url=url.replace("latitude", "1")
+    url=url.replace("longitude", "1")
+    req = urllib2.urlopen(url)
+    result = req.read()
+    r = json.loads(result)
+    for key in r:
+        print key[0]
+        if key[0] == "m":
+            main = key
+            temperature = main["temp"]
+            print "HELLO"
+            return render_template("climate.html",temperature=temperature)
+    print "UGHH"
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
